24d990b9b730da469a8d6787b29b989e5b94db6b
PatrykZymelka <patrykzymelka@op.pl>
2023-07-01 20:08:25 +0200

    AFK_0.5
    A first draft of the bot, covering the base of the program.

---
+++ src/GUI.java
@@ -0,0 +1,131 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI extends JFrame {
+    public JPanel MainPanel;
+    private JButton StartButton;
+    private JProgressBar Progress;
+    private JButton StopButton;
+    private JComboBox TimeMode ;
+    private JComboBox AFKMode;
+    private JLabel timeLabel;
+    private JButton button1;
+    boolean started = false;
+    int elapsedTime = 3580000;
+    int minutes = 0;
+    int seconds = 0;
+    String seconds_string = String.format("%02d", seconds);
+    String minutes_string = String.format("%02d", minutes);
+
+    public GUI() {
+        StartButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(started ==false){
+                    started = true;
+                    StartButton.setText("Stop Process");
+                    start();
+                }
+                else{
+                    started=false;
+                    StartButton.setText("Start Process");
+                    stop();
+                }
+            }
+        });
+
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(started == true){
+                    started = false;
+                    StartButton.setText("Start Process");
+                    stop();
+                    reset();
+                }
+                else{
+                    reset();
+                }
+            }
+        });
+    }
+
+    public void Initialize() {
+        setContentPane(MainPanel);
+        setTitle("AFK bot");
+        setSize(350, 200);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        String[] TimeModes = {"2 minutes", "1 minute","15 seconds","1 second"};
+        for(int i = 0; i <TimeModes.length; i++) {
+            TimeMode.addItem(TimeModes[i]);
+        }
+
+        String[] AFKmodes = {"Jump", "Left Click", "WASD", "Random"};
+        for(int i = 0; i <AFKmodes.length; i++) {
+            AFKMode.addItem(AFKmodes[i]);
+        }
+        timeLabel.setText(minutes_string+":"+seconds_string);
+    }
+    Timer timer = new Timer(1000, new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            elapsedTime = elapsedTime+1000;
+            minutes = (elapsedTime/60000) % 60;
+            seconds = (elapsedTime/1000) % 60;
+            seconds_string = String.format("%02d", seconds);
+            minutes_string = String.format("%02d", minutes);
+            timeLabel.setText(minutes_string+":"+seconds_string);
+            AFK(elapsedTime);
+        }
+    });
+    void AFK (int ElapsedTime){
+        GUIActionPerformer GAP = new GUIActionPerformer();
+        int T = GAP.TimeConversion((String) TimeMode.getSelectedItem());
+        if(ElapsedTime%T == 0){
+            switch((String) AFKMode.getSelectedItem()){
+                case "Jump":
+                    System.out.println("JUMP");
+                    break;
+
+                case "Left Click":
+                    System.out.println("Left Click");
+                    break;
+
+                case "WASD":
+                    System.out.println("WASD");
+                    break;
+
+                case "Random":
+                    System.out.println("Random");
+                    break;
+
+                default:
+                    System.out.println("Random");
+                    break;
+
+            }
+        }
+
+
+    }
+    void start(){
+        timer.start();
+    }
+
+    void stop(){
+        timer.stop();
+    }
+
+    void reset(){
+        elapsedTime = 0;
+        seconds = 0;
+        minutes = 0;
+        timer.stop();
+        seconds_string = String.format("%02d", seconds);
+        minutes_string = String.format("%02d", minutes);
+        timeLabel.setText(minutes_string+":"+seconds_string);
+    }
+}
---
+++ src/GUIActionPerformer.java
@@ -0,0 +1,30 @@
+public class GUIActionPerformer {
+    public int TimeConversion(String Time){
+        switch (Time) {
+            case "2 minutes":
+                return (2 * 60 * 1000);
+            case "1 minute":
+                return (60 * 1000);
+            case "15 seconds":
+                return (15 * 1000);
+            case "1 second":
+                return (1000);
+            default:
+                return (30 * 1000);
+        }
+    }
+    public String MethodConversion(String Method){
+        switch (Method) {
+            case "Jump":
+                return "J";
+            case "Left Click":
+                return "L";
+            case "WASD":
+                return "W";
+            case "Random":
+                return "R";
+            default:
+                return "J";
+        }
+    }
+}
